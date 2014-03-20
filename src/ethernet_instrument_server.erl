b3b557cb2ee914b026b6b5a9a719a30e6e37f43e
Ben LaRoque <benjamin.laroque@gmail.com>
2014-03-20 13:07:48 -0700

    and then some

--- src/ethernet_instrument_server.erl
+++ src/ethernet_instrument_server.erl
@@ -82,14 +82,7 @@ handle_call({N, Cmd}, From, #state{name=N,
     io:format(["IP:",IP," Port:",integer_to_list(Port),"\n"]),
     io:format(["\n***************************************\n"]),
     %{ok, Socket} = gen_tcp:connect(IP, Port, [binary, {socket, 0}]),
-    {Con, Socket} = gen_tcp:connect(IP, Port, [binary, {packet, 0}]),
-    case Con of
-        ok ->
-            io:format(Socket);
-        error ->
-            io:format("got an error:\n"),
-            io:format(Socket)
-    end,
+    {ok, Socket} = gen_tcp:connect(IP, Port, [binary, {packet, 0}]),
     io:format("connection made\n"),
     NewSockets = lists:append(Sockets, {Socket, [], From}),
     io:format("list appended\n"),
