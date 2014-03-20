6cb9153970e6d3a0b04209e7582a45701854438d
Ben LaRoque <benjamin.laroque@gmail.com>
2014-03-20 12:37:37 -0700

    renamed lan_server

--- src/lan_sup.erl
+++ src/lan_sup.erl
@@ -25,7 +25,7 @@ start_link() ->
 
 init([]) ->
     io:format("initting\r\n"),
-    Dmm = {ethernet_instrument_server,
+    Dmm = {dmm,
            {ethernet_instrument_server, start_link, [dmm, "10.0.0.60", 5025, agilent_34460]},
            permanent, 5000, worker, [ethernet_instrument_server]},
     {ok, { {one_for_one, 5, 10}, [Dmm]} }.
