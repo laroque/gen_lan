f6783ea630f0bfa42a9263c64c34a8a504add805
Ben LaRoque <benjamin.laroque@gmail.com>
2014-03-19 22:28:34 -0700

    added a supervisor

---
+++ src/lan_sup.erl
@@ -0,0 +1,32 @@
+
+-module(lan_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    io:format("initting\r\n"),
+    Dmm = {dmm,
+           {ethernet_instrument_server, start_link, [dmm, "10.0.0.60", 5025]},
+           permanent, 5000, worker, [ethernet_instrument_server]},
+    {ok, { {one_for_one, 5, 10}, [Dmm]} }.
+
