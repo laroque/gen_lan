6b959da043f030ea05402dfaa69383b19a0f7348
Ben LaRoque <benjamin.laroque@gmail.com>
2014-03-20 22:04:31 -0700

    raw command

--- src/agilent_34460.erl
+++ src/agilent_34460.erl
@@ -10,6 +10,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Required callbacks
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+send_command({raw, Value}, Socket) ->
+    gen_lan_device:send(Value, Socket);
 send_command({read}, Socket) ->
     gen_lan_device:send("READ?", Socket);
 send_command({status}, Socket) ->
@@ -23,7 +25,9 @@ send_command({trig_sour}, Socket) ->
     gen_lan_device:send("TRIG:SOUR?", Socket);
 send_command({trig_sour, Value}, Socket) ->
     V = gen_lan_device:arg_to_binary(Value),
-    gen_lan_device:send(["TRIG:SOUR ",V], Socket).
+    gen_lan_device:send(["TRIG:SOUR ",V], Socket);
+send_command({_Cmd}, _Socket) ->
+    {error, command_unknown}.
 
 parse_message(Socket, Msg) ->
     gen_lan_device:process_response(Socket, Msg).
